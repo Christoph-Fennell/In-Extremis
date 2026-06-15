5a9a9e656a623b69e569e4da2b1cbd51f9f10874
FennellChris <cjf169@scarletmail.rutgers.edu>
2026-06-15 15:01:43 -0400

    Fixed Instagram link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 
 const socialLinks = [
   { label: 'LinkedIn',  href: 'https://linkedin.com',  icon: LinkedInIcon  },
-  { label: 'Instagram', href: 'https://instagram.com', icon: InstagramIcon },
+  { label: 'Instagram', href: 'https://instagram.com/in_extremis_consulting', icon: InstagramIcon },
   { label: 'X',         href: 'https://x.com/inextremisbis?s=11',         icon: XIcon         },
   { label: 'YouTube',   href: 'https://youtube.com',   icon: YouTubeIcon   },
 ];
