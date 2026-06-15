a7885d287a4faae8d4648104eb37d4defea6658b
FennellChris <cjf169@scarletmail.rutgers.edu>
2026-06-15 11:40:01 -0400

    Added GBRS image

--- src/components/home/LogoStripSection.tsx
+++ src/components/home/LogoStripSection.tsx
@@ -26,7 +26,7 @@ const logos: LogoItem[] = [
   { name: 'Client 1', src: '/images/logos/Colt_idmLsNB0xV_3.jpeg', width: 160, height: 48 },
   { name: 'Client 2', src: '/images/logos/Agilite Logo.png', width: 160, height: 48 },
   { name: 'Client 3', src: '/images/logos/hesco_logo.jpg', width: 160, height: 48 },
-  { name: 'Client 4', src: '/images/logos/client-4.svg', width: 160, height: 48 },
+  { name: 'Client 4', src: '/images/logos/GBRS_Group_idgdVDrzmQ_0.png', width: 160, height: 48 },
   { name: 'Client 5', src: '/images/logos/client-5.svg', width: 160, height: 48 },
   { name: 'Client 6', src: '/images/logos/client-6.svg', width: 160, height: 48 },
 ];
